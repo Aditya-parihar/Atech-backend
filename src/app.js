c6bfbb3c5b6305738bfa12147fec69f9228951ca
Aditya-Parihar <aadiparihar1149@gmail.com>
2025-03-31 00:26:24 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+
+app.use(express.json());
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello, Express is working!");
+// });
+
+
+module.exports = app;
